c1e03959dd85d698bb2f4c111c1a30b2151ca0b2
Ulexus <ulexus@gmail.com>
2015-10-26 00:32:47 -0400

    Add reactive-table

--- client/list.js
+++ client/list.js
@@ -5,11 +5,39 @@ Template.list.helpers({
       },{
          sort: ['name']
       });
+   },
+   'tableSettings': function() {
+      return {
+         fields: [
+            {
+               label: 'Site',
+               key: 'name',
+            },
+            {
+               label: 'Added',
+               key: 'added',
+               fn: function(val) {
+                  return moment.unix(val).calendar();
+               },
+               sortByValue: true
+            },
+            {
+               label: 'Delete',
+               key: '_id',
+               fn: function(val) {
+                  return new Spacebars.SafeString('<button class="btn delete" data-id='+val+'><i class="fa fa-trash"></i></button>');
+               },
+               sortable: false
+            },
+         ]
+      };
    }
 });
 
 Template.list.events({
-   'click button.delete': function() {
-      Lists.remove(this._id);
+   'click .delete': function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      Lists.remove(e.currentTarget.dataset.id);
    }
 });
